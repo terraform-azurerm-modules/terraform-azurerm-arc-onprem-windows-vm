a7603384c82c53e12bf2b8f6f7302bc658e20923
Richard Cheney <richeney@microsoft.com>
2021-09-22 13:45:35 +0100

    Registry setting for WinRM with local admin ID

--- files/azure_agent_imds.ps1
+++ files/azure_agent_imds.ps1
@@ -9,3 +9,8 @@
 
 Write-Host "Configuring the internal OS firewall to block access to the Azure IMDS (169.254.169.254)."
 New-NetFirewallRule -Name BlockAzureIMDS -DisplayName "Block access to Azure IMDS" -Enabled True -Profile Any -Direction Outbound -Action Block -RemoteAddress 169.254.169.254
+
+# Add registry setting to allow WinRM with local administrator IDs
+# <https://docs.microsoft.com/windows-server/manage/windows-admin-center/support/troubleshooting#i-can-connect-to-some-servers-but-not-others>
+# REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v LocalAccountTokenFilterPolicy /t REG_DWORD /d 1
+New-ItemProperty -Path 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System' -Name  'LocalAccountTokenFilterPolicy' -Value '1' -PropertyType 'DWORD' –Force
